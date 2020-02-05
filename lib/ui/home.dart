54444e223d74be497819a088b112c0dd1307e964
hungryemon <emonshahed@gmail.com>
2020-02-05 21:34:50 +0600

    Initial Commit

---
+++ lib/ui/home.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Bmi extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new BmiState();
+  }
+
+}
+
+class BmiState extends State<Bmi> {
+  final TextEditingController _ageController = new TextEditingController();
+  final TextEditingController _heightController = new TextEditingController();
+  final TextEditingController _weightController = new TextEditingController();
+  double inches = 0.0;
+  double result = 0.0;
+  String _resultReading = '';
+  String _finalResult = '';
+
+  void _calculateBMI() {
+    setState(() {
+      int age = int.parse(_ageController.text);
+      double height = double.parse(_heightController.text);
+      inches = height*12;
+      double weight = double.parse(_weightController.text);
+
+      if((_ageController.text.isNotEmpty || age > 0)
+      && ((_heightController.text.isNotEmpty || inches > 0)
+          && (_weightController.text.isNotEmpty || weight > 0))) {
+        result = weight / (inches * inches)* 703;
+        
+        if (double.parse(result.toStringAsFixed(1)) < 18.5) {
+          _resultReading = 'Underweight';
+        }
+        else if (double.parse(result.toStringAsFixed(1)) >= 18.5 && result < 25) {
+          _resultReading = ' in Great Shape!';
+        }
+        else if (double.parse(result.toStringAsFixed(1)) >= 25.0 && result < 30) {
+          _resultReading = 'Overweight';
+        }
+        else if (double.parse(result.toStringAsFixed(1)) >=30.0) {
+          _resultReading = 'Obese';
+        }
+      }
+      else{
+        result = 0.0;
+      }
+    });
+
+    _finalResult = 'Your BMI: ${result.toStringAsFixed(1)}';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold (
+      appBar: new AppBar(
+        title: new Text('BMI'),
+        centerTitle: true,
+        backgroundColor: Colors.pinkAccent,
+      ),
+      
+      body: new Container(
+        alignment: Alignment.topCenter,
+        child: new ListView(
+            padding: const EdgeInsets.all(2.0),
+          children: <Widget> [
+            new Image.asset('images/bmilogo.png',
+            height: 85.0,
+            width: 75.0,
+            ),
+            new Container(
+              margin: const EdgeInsets.all(3.0),
+//              height: 250.0,
+//              width: 290.0,
+              color: Colors.grey.shade300,
+              child: new Column(
+                children: <Widget>[
+                  new TextField(
+                    controller: _ageController,
+                    keyboardType: TextInputType.number,
+                    decoration: new InputDecoration(
+                      labelText: 'Age',
+                      hintText: 'e.g: 25',
+                        icon: new Icon(Icons.person_outline)
+                    ),
+                  ),
+
+                  new TextField(
+                  controller: _heightController,
+                  keyboardType: TextInputType.number,
+                  decoration: new InputDecoration(
+                      labelText: 'Height in feet',
+                      hintText: 'e.g: 5.7',
+                      icon: new Icon(Icons.insert_chart)
+                    ),
+                  ),
+
+                  new TextField(
+                    controller: _weightController,
+                    keyboardType: TextInputType.number,
+                    decoration: new InputDecoration(
+                        labelText: 'Wight in lbs',
+                        hintText: 'e.g: 180',
+                        icon: new Icon(Icons.line_weight)
+                    ),
+                  ),
+                  new Padding(padding: new EdgeInsets.all(10.6)),
+
+                  new Container(
+                    alignment: Alignment.center,
+                    child: new RaisedButton(
+                    onPressed: _calculateBMI,
+                    color: Colors.pinkAccent,
+                    child: new Text('Calculate'),
+                    textColor: Colors.white,
+                    ),
+                  )
+
+
+                ],
+              ),
+            ),
+            new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Text('$_finalResult',
+                style: new TextStyle(
+                   color: Colors.blueAccent,
+                  fontWeight: FontWeight.w600,
+                  fontSize:  20.0,
+                  fontStyle: FontStyle.italic
+                ),
+                ),
+                new Padding(padding: const EdgeInsets.all(5.0)),
+
+                new Text('You are $_resultReading',
+                  style: new TextStyle(
+                      color: Colors.redAccent,
+                      fontWeight: FontWeight.w600,
+                      fontSize:  20.0,
+                      fontStyle: FontStyle.italic
+                  ),
+                ),
+
+
+              ],
+
+
+
+            ),
+          ]
+        )
+      ),
+    );
+  }
+
+}
